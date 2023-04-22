fd195f0a8a642e77d43d9ff6d48df02259f3fb31
Jimena Hernandez Garcia <72419649+JimenaHG5@users.noreply.github.com>
2023-04-22 16:04:14 -0600

    Add files via upload

---
+++ ayuda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ayuda
+echo -e "\033[32m---- ayuda ----\n> Este comando  brinda informacion sobre los comandos dentro del programa.\033[0m"
+echo -e "\033[32m---- busqueda ----\n> Permite buscar un archivo especifico en un directorio especifico, te solicitara primero el nombre del directorio, y luego el del archivo\033[0m"
+echo -e "\033[32m---- infosis ----\n> Muestra la informacion basica del sistema operativo, Memoria RAM, version del sistema y arquitectura\033[0m"
+echo -e "\033[32m---- ahorcado----\n> Abre un juego de ahorcado dentro de la terminal.\033[0m"
+echo -e "\033[32m---- fecha----\n> te da la hora y fecha actual .\033[0m"
+echo -e "\033[32m---- creditosh ----\n> creditos al programador .\033[0m"
+echo -e "\033[32m---- mp3 ----\n> despliega un reproductor de musica  .\033[0m"
